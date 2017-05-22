51e853172406edf3b79e1139750f5f0c540d6c6b
a05h <a117q35mt@gmail.com>
2017-05-21 20:52:05 -0400

    update

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 var http = require('http'),
     fs = require('fs'),
     express = require('express'),
-    router = require('./modules/router');
+    route = require('./modules/router');
 
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('./public/styles'));
+app.use(express.static('./public'));
 
-//app.get('/', (req, res) => {
-//  res.render('index', {thisPage: 'index'});
-//  console.log(`Request: ${req.url}, status: ${res.statusCode} - ${res.statusMessage}`);
-//});
-//
-//app.get('/contacts', (req, res) => {
-//  res.render('contacts', {qs: req.query});
-//  console.log(`Request: ${req.url}, status: ${res.statusCode} - ${res.statusMessage}`);
-//  console.log(req.body);
-//});
-//
-//app.post('/contacts', urlencodedParser, (req, res) => {
-//  res.render('contacts', {qs: req.query});
-//  console.log(`Request: ${req.url}, status: ${res.statusCode} - ${res.statusMessage}`);
-//  console.log(req.body);
-//});
-//
-
-router(app);
+route(app);
 
 app.listen(3000, '127.0.0.1');
-
 console.log('Listening port: 3000');
